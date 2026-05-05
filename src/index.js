0d665c7a1947c24302de115fe2f55e8b91a2f728
MahanteshNaikD <naikd9164@gmail.com>
2026-05-05 21:28:26 +0530

    bug fixes

--- src/index.js
+++ src/index.js
@@ -83,7 +83,11 @@ async function sendHeartbeat(session) {
 
 async function notifyStreamEnded(streamId, exitCode = 0) {
   try {
-    console.log('gcsPayloadForWebhook', gcsPayloadForWebhook(streamId));
+    console.log('gcsPayloadForWebhook', {
+      stream_id: streamId,
+      exit_code: exitCode,
+      ...gcsPayloadForWebhook(streamId),
+    });
     await postServerA('/internal/worker/stream-ended', {
       stream_id: streamId,
       exit_code: exitCode,
